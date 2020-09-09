9395085be8f39e7554452a66409bbc1658537653
Rob Lopez <rob.lopez@gmail.com>
2020-09-09 14:19:17 -0700

    Updates to exact iphone on xcode for package
    Xcode still not working with current build

---
+++ nativeInit/nativePlayground/nativePlayground/nativePlayground/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  nativePlayground
+//
+//  Created by Robert Lopez on 9/9/20.
+//  Copyright © 2020 Robert Lopez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
